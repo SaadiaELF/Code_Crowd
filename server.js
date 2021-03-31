330dd2d929ad6867f7cb62c1b986c00f88ac9d53
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-31 13:41:08 +0100

    updated code

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const sequelize = require('./config/connection');
 
 // Require app
-const app = require(express());
+const app = express();
 
 // Port
 const PORT = process.env.PORT || 8000;
 
 // Make app listen on port
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+    app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
 });
