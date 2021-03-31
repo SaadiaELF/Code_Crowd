17ee76e9ec73154f432aaab237f7ad6ae82731e4
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-31 13:41:19 +0100

    updated models

--- models/User.js
+++ models/User.js
@@ -7,7 +7,56 @@ class User extends Model {}
 // Init and create table
 User.init(
     {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        first_name: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        last_name: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        email: {
 
+        },
+        password: {
+
+        },
+        programming_languages: {
+
+        },
+        city: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        country: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        // date_of_birth: {
+
+        // },
+        // friend_id: {
+
+        // },
+        // post_id: {
+
+        // },
+        // comment_id: {
+
+        // }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
     }
 );
 
