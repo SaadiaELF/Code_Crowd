caf2452673ccb20658c6e083604871296b7c9905
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-31 12:30:41 +0100

    added column data

--- models/Post.js
+++ models/Post.js
@@ -2,19 +2,25 @@
 const { Model, Datatypes } = require('sequelize');
 
 // Create class
-class Post extends Model {}
+class Post extends Model { }
 
 // Init and create table
 Post.init(
     {
         id: {
-
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
         },
         content: {
-
+            type: Datatypes.STRING,
+            allowNull: false
         },
         date: {
-
+            type: Datatypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
         },
         like: {
 
