281d5885bf6d7c782ee2fba7ddd4b78f86201102
Saadia El fekak <saadiaelfekak@gmail.com>
2021-04-01 20:05:56 +0100

    Adding profile get route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,27 +4,45 @@ const { User, Post, File, Image, Comment } = require('../models');
 
 // DEFINE ALL ROUTES BELOW
 
-router.get('/', async (req, res) => {
+// router.get('/', async (req, res) => {
+//     try {
+//         // Get all posts and JOIN with user data
+//         const postData = await Post.findAll({
+//             include: [
+
+//             ]
+//         });
+
+//         // Serialize data so the template can read it
+
+
+//         // Pass serialized data and session flag into template
+
+//     }
+
+//     catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//     }
+// })
+
+router.get('/profile', async (req, res) => {
     try {
-        // Get all posts and JOIN with user data
         const postData = await Post.findAll({
+          
             include: [
-
-            ]
+                {
+                    model: User,
+                    attributes: ['id', 'first_name', 'last_name'],
+                }],
         });
-
-        // Serialize data so the template can read it
-
-
-        // Pass serialized data and session flag into template
-
+        const posts = postData.map((post) => post.get({ plain: true }));
+        console.log(posts)
+        res.render('profile', { posts });
     }
-
     catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+        res.status(500).json("Error: Cannot render the page");
     }
 })
-
 // Export the module
 module.exports = router;
