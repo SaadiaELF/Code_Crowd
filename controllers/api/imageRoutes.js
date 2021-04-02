9d29203f540206ff79a0b047eecd1fbb6cd8659f
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-04-02 13:10:49 +0100

    added routes for getting content on page

--- controllers/api/imageRoutes.js
+++ controllers/api/imageRoutes.js
@@ -3,5 +3,23 @@ const router = require('express').Router();
 const { Image } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// Get images
+router.get('/', async (req, res) => {
+    try {
+        const commentData = await Image.findAll();
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No image found with this id' });
+        }
+    }
+
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Add a new image in a post --> ?
+
 // Export module
 module.exports = router;
