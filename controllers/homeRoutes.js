9128e5e008b5b111acee545d14848835cbb4c5b7
Saadia El fekak <saadiaelfekak@gmail.com>
2021-04-03 18:02:11 +0100

    Adding profile routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -29,20 +29,48 @@ const { User, Post, File, Image, Comment } = require('../models');
 router.get('/profile', async (req, res) => {
     try {
         const postData = await Post.findAll({
-          
+            where: {
+                user_id: 1
+            },
             include: [
                 {
                     model: User,
-                    attributes: ['id', 'first_name', 'last_name'],
+                    attributes: ['id', 'first_name', 'last_name', 'profile_picture'],
                 }],
         });
+
         const posts = postData.map((post) => post.get({ plain: true }));
         console.log(posts)
-        res.render('profile', { posts });
+         res.render('profile', {posts});
     }
     catch (err) {
         res.status(500).json("Error: Cannot render the page");
     }
 })
+
+router.put('/profile/:id', async (req, res) => {
+    // update post by id
+    try {
+        console.log(req.body.imageUrl);
+        const userData = await User.update({
+            profile_picture: req.body.imageUrl
+        },
+            {
+                where: {
+                    id: "1"
+                },
+            });
+
+        if (!userData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+
+        res.status(200).render('profile');
+    } catch (err) {
+        res.status(500).json("Error: Cannot update the post");
+    }
+});
+
 // Export the module
 module.exports = router;
