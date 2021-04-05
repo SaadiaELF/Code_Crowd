61102e37118b1388c48957a755f4fa683c88f736
Saadia El fekak <saadiaelfekak@gmail.com>
2021-04-05 17:10:46 +0100

    Adding delete post function

---
+++ public/js/delete-post.js
@@ -0,0 +1,22 @@
+const delButtonHandler = async (event) => {
+  if (event.target.matches('#delete-btn') && event.target.hasAttribute('data-id')) {
+
+    // Collect values from the delete button
+    const id = event.target.getAttribute('data-id');
+    console.log(id);
+    // Send a POST request to the API endpoint
+    const response = await fetch(`/api/post/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the dashboard page
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document 
+  .addEventListener('click', delButtonHandler);
