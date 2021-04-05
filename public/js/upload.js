159279aa7e802424ce8d61a2865059a881e4d822
Saadia El fekak <saadiaelfekak@gmail.com>
2021-04-06 00:05:05 +0100

    Adding add-comment function

--- public/js/upload.js
+++ public/js/upload.js
@@ -27,19 +27,20 @@ document.getElementById("upload_widget").addEventListener("click", async functio
 
 }, false);
 
-var myWidget = cloudinary.createUploadWidget({
+var myWidget2 = cloudinary.createUploadWidget({
     cloudName: 'birminghambcs',
     uploadPreset: 'gbw7i3gr'
 }, (error, result) => {
     if (!error && result && result.event === "success") {
+        const screenshotUrl = result.info.secure_url;
         document.getElementById("img_preview").classList.remove("hide");
-        document.getElementById("img_preview").src = result.info.secure_url;
-        document.getElementById('screenshot_url').innerHTML = result.info.secure_url
+        document.getElementById("img_preview").src = screenshotUrl;
+        document.getElementById('screenshot_url').innerHTML = screenshotUrl;
     }
 }
 )
 document.getElementById("upload_img_widget").addEventListener("click", async function () {
-    await myWidget.open();
+    await myWidget2.open();
 
 }, false);
 
