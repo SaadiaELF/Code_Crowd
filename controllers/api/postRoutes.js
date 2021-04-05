16f2fe434fdc767597d2e45e717de2165706d97a
Saadia El fekak <saadiaelfekak@gmail.com>
2021-04-05 19:04:50 +0100

    Adding dislike route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -21,7 +21,7 @@ router.post('/', async (req, res) => {
 
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/like/:id', async (req, res) => {
     // update post by id
       try {
         // console.log(req.body.imageUrl);
@@ -45,6 +45,29 @@ router.put('/:id', async (req, res) => {
     }
 });
 
+router.put('/dislike/:id', async (req, res) => {
+    // update post by id
+      try {
+        // console.log(req.body.imageUrl);
+        const postData = await Post.update({
+            dislike: req.body.dislike
+        },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json("Error: Cannot update the post");
+    }
+});
 // Delete an existing post, by specific id
 router.delete('/:id', async (req, res) => {
     // delete post by id
