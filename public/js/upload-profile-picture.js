f4b8ea025adf288890d5f4226d9f8a1e35cbb31a
Saadia El fekak <saadiaelfekak@gmail.com>
2021-04-06 11:54:05 +0100

    Upadting functions

--- public/js/upload-profile-picture.js
+++ public/js/upload-profile-picture.js
@@ -26,21 +26,3 @@ document.getElementById("upload_widget").addEventListener("click", async functio
     await myWidget.open();
 
 }, false);
-
-var myWidget2 = cloudinary.createUploadWidget({
-    cloudName: 'birminghambcs',
-    uploadPreset: 'gbw7i3gr'
-}, (error, result) => {
-    if (!error && result && result.event === "success") {
-        const screenshotUrl = result.info.secure_url;
-        document.getElementById("img_preview").classList.remove("hide");
-        document.getElementById("img_preview").src = screenshotUrl;
-        document.getElementById('screenshot_url').innerHTML = screenshotUrl;
-    }
-}
-)
-document.getElementById("upload_img_widget").addEventListener("click", async function () {
-    await myWidget2.open();
-
-}, false);
-
