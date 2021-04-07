74c0865f75de8e63adc763e8a96b1eb95206599f
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-04-07 21:09:14 +0100

    added comments and formatted dpcument

--- public/js/add-friend.js
+++ public/js/add-friend.js
@@ -1,29 +1,31 @@
+// Add friend to the list of friends in user profile, on the click of the Add Friend button
 const addFriendHandler = async (event) => {
-    event.preventDefault();
-  
-    // Collect values from the new post form
-    const friend_id = event.target.getAttribute('data-id');
-​
-    if (friend_id ) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/friend', {
-        method: 'POST',
-        body: JSON.stringify({friend_id}),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        // If successful, redirect the browser to the dashboard page
-        document.location.replace('/friends');
-      } else {
-        alert('Already added as a friend !');
-      }
+  event.preventDefault();
+
+  // Collect values from the new post form
+  const friend_id = event.target.getAttribute('data-id');
+
+  if (friend_id) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/friend', {
+      method: 'POST',
+      body: JSON.stringify({ friend_id }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the dashboard page
+      document.location.replace('/friends');
+    } else {
+      alert('Already added as a friend !');
     }
-​
-  };
-​
-    var friendBtns = document.querySelectorAll('#addFriend');
-  
-    for (var i = 0 ; i < friendBtns.length ; i++){
-      friendBtns[i].addEventListener('click', addFriendHandler);
-    }
+  }
+
+};
+
+// Apply above function to ALL FRIEND ACCOUNTS in the search results, by looping through all of them (from the user.handlebars)
+var friendBtns = document.querySelectorAll('#addFriend');
+
+for (var i = 0; i < friendBtns.length; i++) {
+  friendBtns[i].addEventListener('click', addFriendHandler);
+}
