e15ca060a1125e2a6e5d22fa9f0bba8308182e4c
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-04-08 12:39:12 +0100

    edited db

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,9 @@ DROP DATABASE IF EXISTS socialmedia_db;
 
 -- CREATE DATABASE
 CREATE DATABASE socialmedia_db;
+
+SELECT * FROM user;
+
+INSERT INTO `` (`id`,`first_name`,`last_name`,`email`,`password`,`programming_languages`,`city`,`country`,`profile_picture`,`date_of_birth`) VALUES ('3b7e339c-d6df-464c-b070-b42c3ea144f7','Saadia','Elf','saadia@gmail.com','$2b$10$9ct5blSzc5eu4pivEE9Nr.zj5DQCS4X0Ci8IvEV/DDME4ndJo87jq','CSS, HTML, JS','London','UK','https://res.cloudinary.com/birminghambcs/image/upload/v1617570986/user_a78tmx.png','1996-08-17 00:00:00');
+INSERT INTO `` (`id`,`first_name`,`last_name`,`email`,`password`,`programming_languages`,`city`,`country`,`profile_picture`,`date_of_birth`) VALUES ('83e7b383-3a54-4fa4-a2c8-18f87565051d','Youssef','Ahmed','youssef@gmail.com','$2b$10$s7NhFWSDB7NBAtdxbNogguVTwUN7.w3Wch.AxWRrnAe0XVZJNrRGy','NodeJs , JQuery','Birmingham','UK','https://res.cloudinary.com/birminghambcs/image/upload/v1617570986/user_a78tmx.png','1995-04-06 00:00:00');
+INSERT INTO `` (`id`,`first_name`,`last_name`,`email`,`password`,`programming_languages`,`city`,`country`,`profile_picture`,`date_of_birth`) VALUES ('ed113375-1b71-4d98-8d19-e30b82fe6b8a','Sara','Elf','sara@gmail.com','$2b$10$inxsqM.2WYEEyOS6ft7lQ.Q5RvyzKC/PRnEnZ0SH4xfj6.cuzKNVu','CSS, HTML, JS, NodeJS','Birmingham','UK','http://res.cloudinary.com/birminghambcs/image/upload/v1617878421/kiusgmavsvciwugd8ipb.png','2001-10-11 00:00:00');
