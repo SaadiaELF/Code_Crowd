d602abe8649d910238f5de103f8bc37c2a81b078
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-04-08 11:44:48 +0100

    Edited redirect page

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,11 @@
 const withAuth = (req, res, next) => {
-    // If the user is not logged in, redirect the request to the login route
-    if (!req.session.user_id) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  // Export module
-  module.exports = withAuth; 
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.user_id) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
+
+// Export module
+module.exports = withAuth;
