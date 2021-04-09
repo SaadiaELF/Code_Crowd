3cb2e93c79dcd4265baee80e360d97d9718f1a79
Saadia El fekak <saadiaelfekak@gmail.com>
2021-04-09 01:09:09 +0100

    Adding seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Post, Comment, File, Image } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
 const commentData = require('./commentData.json');
-const fileData = require('./fileData.json');
-const imageData = require('./imageData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -19,22 +17,12 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-  // await Comment.bulkCreate(commentData, {
-  //   individualHooks: true,
-  //   returning: true,
-  // });
+
+  await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });
   
-  // await File.bulkCreate(fileData, {
-  //   individualHooks: true,
-  //   returning: true,
-  // });
-
-  // await Image.bulkCreate(imageData, {
-  //   individualHooks: true,
-  //   returning: true,
-  // });
-
-
   process.exit(0);
 };
 
