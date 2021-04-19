8c74834bc7a40ee0deef374ac79a3f7d37b670cb
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-04-19 19:52:57 +0100

    added compression

--- server.js
+++ server.js
@@ -37,23 +37,6 @@ const sess = {
 // compress all responses
 app.use(compression());
 
-// server-sent event stream
-app.get('/events', function (req, res) {
-    res.setHeader('Content-Type', 'text/event-stream')
-    res.setHeader('Cache-Control', 'no-cache')
-
-    // send a ping approx every 2 seconds
-    var timer = setInterval(function () {
-        res.write('data: ping\n\n')
-
-        // !!! this is the important part
-        res.flush()
-    }, 2000)
-
-    res.on('close', function () {
-        clearInterval(timer)
-    })
-})
 
 app.use(session(sess));
 
